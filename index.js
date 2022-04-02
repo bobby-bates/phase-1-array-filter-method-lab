fc33664d5502db385cf3390f9ab550a5f72b08a3
Bobby Bates <robertfbatesjr@gmail.com>
2022-04-02 14:27:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+
+const findMatching = (arr, str) => {
+    return arr.filter(el => el.toLowerCase() === str.toLowerCase())
+}
+
+const fuzzyMatch = (arr, str) => {
+    return arr.filter(el => el.startsWith(str))
+}
+
+const matchName = (arr, str) => {
+    debugger
+    return arr.filter(el => el.name === str)
+}
